aa015e806aca8c9f7ceaff24252db55bab68ae15
Jonas Schoenherr <jonas.schoenherr@avira.com>
2022-10-18 15:07:29 +0200

    Make basic changes suggested by PEP-8

--- Tertis-1/NEW-game.py
+++ Tertis-1/NEW-game.py
@@ -16,7 +16,7 @@ clock = pygame.time.Clock()
 
 grid = [pygame.Rect(x * Tile, y * Tile, Tile, Tile) for x in range(W) for y in range(H)]
 
-#Figures are render via coordinates in a grid
+# Figures are render via coordinates in a grid
 figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
                [(0, -1), (-1, -1), (-1, 0), (0, 0)],
                [(-1, 0), (-1, 1), (0, 0), (0, -1)],
@@ -26,22 +26,22 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
                [(0, 0), (0, -1), (0, 1), (-1, 0)]]
 
 
-figures = [[pygame.Rect(x + W// 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 figure_rect = pygame.Rect(0, 0, Tile - 2, Tile - 2)
-field = [[0 for i in range(W)] for j in range (H)]
+field = [[0 for i in range(W)] for j in range(H)]
 anim_count, anim_speed, anim_limit = 0, 60, 2000
-figure =  deepcopy(choice(figures))
+figure = deepcopy(choice(figures))
 
 
 bg = pygame.image.load('bg.png').convert()
-game_bg = pygame.image.load('bg.2.jpeg').convert()
+game_bg = pygame.image.load('bg.png').convert()
 
-#generating random colour values for figures
-get_colour = lambda :(randrange(30, 256), randrange(30, 256), randrange(30, 256))
+# Generating random colour values for figures
+get_colour = lambda: (randrange(30, 256), randrange(30, 256), randrange(30, 256))
 
-#Figures and fonts
-figure, next_figure =  deepcopy(choice(figures)), deepcopy(choice(figures))
-colour, next_colour  = get_colour(), get_colour()
+# Figures and fonts
+figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
+colour, next_colour = get_colour(), get_colour()
 
 main_font = pygame.font.SysFont('ttf', 105, bold=True,)
 font = pygame.font.SysFont('tahoma', 35, bold=True)
@@ -52,19 +52,19 @@ title_score = font.render('Score:', True, pygame.Color('green'))
 title_HighScore = font.render('Highscore:', True, pygame.Color('green'))
 
 
-#Score
+# Score
 ''' variable for scroe and lines
 and dictionary for accrual points by the number of collected lines'''
 score, line = 0, 0
 scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
 
 
-'''Return a false value when the figure has reached the bottom
-and if another figure is already in the feild'''
 def game_borders():
+    '''Return a false value when the figure has reached the bottom
+    and if another figure is already in the feild'''
     if figure[i].x < 0 or figure[i].x > W - 1:
         return False
-    elif figure[i].y > H -1 or field[figure[i].y][figure[i].x]:
+    elif figure[i].y > H-1 or field[figure[i].y][figure[i].x]:
         return False
     return True
 
@@ -88,17 +88,16 @@ while True:
     Highscore = get_Highscore()
     dx, rotate = 0, False
 
-    #place backgrounds and grid
-    win.blit(bg, (0,0))
-    win.blit(game_sc, (10,50))
-    game_sc.blit(game_bg, (0,0))
+    # Place backgrounds and grid
+    win.blit(bg, (0, 0))
+    win.blit(game_sc, (10, 50))
+    game_sc.blit(game_bg, (0, 0))
 
-    #Animate full line removal
+    # Animate full line removal
     for i in range(line):
         pygame.time.wait(200)
 
-
-    #controls
+    # Controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
@@ -112,8 +111,7 @@ while True:
             elif event.key == pygame.K_UP:
                 rotate = True
 
-
-    #move x
+    # Move x
     figure_old = deepcopy(figure)
     for i in range(4):
         figure[i].x += dx
@@ -121,8 +119,7 @@ while True:
             figure = deepcopy(figure_old)
             break
 
-
-    #move y
+    # Move y
     '''move y by using a counter that increases by the value
     of the animation speed and when the limit is reached we increase the y coordinate by 1'''
     anim_count += anim_speed
@@ -130,38 +127,37 @@ while True:
         anim_count = 0
         figure_old = deepcopy(figure)
         for i in range(4):
-         figure[i].y += 1
-         if not game_borders():
-             for i in range(4):
-                 field[figure_old[i].y][figure_old[i].x] = colour
-             figure, colour = next_figure, next_colour
-             next_figure, next_colour = deepcopy(choice(figures)), get_colour()
-             anim_limit = 2000
-             break
+            figure[i].y += 1
 
+            if not game_borders():
+                for i in range(4):
+                    field[figure_old[i].y][figure_old[i].x] = colour
+                figure, colour = next_figure, next_colour
+                next_figure, next_colour = deepcopy(choice(figures)), get_colour()
+                anim_limit = 2000
+                break
 
-    #rotate
+    # Rotate
     '''Center of rotation, rotate figure by 90 degrees this is done by
     calculating the difference between the coordinates of the  figure and
     the center of rotation and adding the differeance to the x coordinate'''
     center = figure[0]
     figure_old = deepcopy(figure)
     if rotate:
-     for i in range(4):
-         x = figure[i].y - center.y
-         y = figure[i].x - center.x
-         figure[i].x = center.x - x
-         figure[i].y = center.y + y
-         if not game_borders():
-             figure = deepcopy(figure_old)
-             break
+        for i in range(4):
+            x = figure[i].y - center.y
+            y = figure[i].x - center.x
+            figure[i].x = center.x - x
+            figure[i].y = center.y + y
+            if not game_borders():
+                figure = deepcopy(figure_old)
+                break
 
-
-    #Check and tally lines
+    # Check and tally lines
     '''for each line collected, add to score depending on accruled lines
     for each line collected increase speed by 3'''
-    line, lines = H -1, 0
-    for row in range(H -1, -1, -1):
+    line, lines = H-1, 0
+    for row in range(H-1, -1, -1):
         count = 0
         for i in range(W):
             if field[row][i]:
@@ -173,46 +169,40 @@ while True:
             anim_speed += 3
             lines += 1
 
-    #tally and blit score
+    # Tally and blit score
     score += scores[lines]
     win.blit(title_score, (490, 480))
     win.blit(font.render(str(score), True, pygame.Color('yellow')), (525, 525))
 
-
-    #draw grid
+    # Draw grid
     [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
 
-
-
-    # draw figure by iterating through the coordinates and multiplying the x and y by the tile
+    # Draw figure by iterating through the coordinates and multiplying the x and y by the tile
     for i in range(4):
         figure_rect.x = figure[i].x * Tile
         figure_rect.y = figure[i].y * Tile
         pygame.draw.rect(game_sc, colour, figure_rect)
 
-
-    #draw next figure
+    # Draw next figure
     for i in range(4):
         figure_rect.x = next_figure[i].x * Tile + 380
         figure_rect.y = next_figure[i].y * Tile + 185
         pygame.draw.rect(win, next_colour, figure_rect)
 
-
-    # draw feild
+    # Draw feild
     for y, raw in enumerate(field):
-        for x , col in enumerate(raw):
+        for x, col in enumerate(raw):
             if col:
                 figure_rect.x, figure_rect.y = x * Tile, y * Tile
                 pygame.draw.rect(game_sc, col, figure_rect)
     win.blit(title_nextShape, (445, 130))
 
-
-    #titles
+    # Titles
     win.blit(title_tetris, (405, 15))
     win.blit(title_HighScore, (455, 585))
     win.blit(font.render(Highscore, True, pygame.Color('yellow')), (525, 625))
 
-    #GAME OVER
+    # GAME OVER
     for i in range(W):
         if field[0][i]:
             set_Highscore(Highscore, score)
@@ -221,7 +211,7 @@ while True:
             score = 0
             for i_rect in grid:
                 pygame.draw.rect(game_sc, get_colour(), i_rect)
-                win.blit(game_sc, (10,50))
+                win.blit(game_sc, (10, 50))
                 pygame.display.flip()
                 clock.tick(2000)
 
