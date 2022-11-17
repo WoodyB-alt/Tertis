34f8599bf2a8b001f4b6cb714ea683ae3ea7df67
Jonas Schoenherr <jonas.schoenherr@avira.com>
2022-11-17 14:59:44 +0100

    Add basic api scaffolding for Tertis Player simulation

---
+++ Tertis-1/TertisPlayerStrategies.py
@@ -0,0 +1,13 @@
+"""
+TERTIS Player strategies library
+"""
+
+
+class TertisPlayerStrategy:
+    """Base interface for a Tertis player startegy."""
+    pass
+
+
+class StraightDown(TertisPlayerStrategy):
+    """Implement a 'straight down' Tertis player strategy."""
+    pass
