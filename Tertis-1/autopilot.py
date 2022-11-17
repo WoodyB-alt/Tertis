2f64e49e92d1d441aade32ed05b6e802a40c2dfa
Jonas Schoenherr <jonas.schoenherr@avira.com>
2022-11-17 14:22:02 +0100

    Add autopilot file with description
    Starting point for all efforts related to implementing a Tertis player
    computer simulation.

---
+++ Tertis-1/autopilot.py
@@ -0,0 +1,11 @@
+"""
+TERTIS Autopilot
+
+Player simulation for the Tertis app.
+
+When run, this program will simulate a player of the Tertis app
+by interacting with the Tertis API and executing configured strategies.
+"""
+
+
+
